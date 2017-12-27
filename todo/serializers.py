305cc1d19fcb1377607146a55f9dfd869607f01f
subham <subham.bhattacharjee1@gmail.com>
2017-12-27 15:41:32 +0530

    remove everything

--- todo/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from todo.models import TodoElements
-
-class TodoSerializers(serializers.ModelSerializer):
-	class Meta:
-		model = TodoElements
-		fields = 'id', 'todo_text', 'done'
